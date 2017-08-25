39bbdfd3f62f6ed19bc401811496730bcf3eb9bd
WanderingBrooks <cs.jasonbrooks@gmail.com>
2017-08-25 18:00:59 -0400

    proxy creation beginning

--- handler.js
+++ handler.js
@@ -1,16 +1,41 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+const request = require('request');
+
+module.exports.malProxy = (event, context, callback) => {
+  
   const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      statusCode: 200
   };
 
-  callback(null, response);
+  const type = event.queryStringParameters.type;
+  const suffix = {
+    verify: `myanimelist.net/api/account/verify_credentials.xml`,
+    add: `https://myanimelist.net/api/animelist/add/`,
+    update: `https://myanimelist.net/api/animelist/update/`, 
+    delete: `https://myanimelist.net/api/animelist/delete/`
+  };
+  
+  const params = {
+    url: `https://${encodeURIComponent(event.queryStringParameters.username)}:${encodeURIComponent(event.queryStringParameters.password)}@${suffix[type]}`
+  };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  if (type != 'verify') { 
+    params.url += `${event.queryStringParameters.id}.xml`
+
+    request.post(params, {form: {data: event.queryStringParameters.data}}, function (error, response, body) {
+      console.log(error);
+      console.log(response);
+      console.log(body);
+    });
+  } 
+  else {
+    request(params, function (error, res, body) {
+    console.log(error);
+    console.log(body);
+    response['body'] = body;
+    console.log(response);
+    callback(null, response);
+  });
+  }
 };
