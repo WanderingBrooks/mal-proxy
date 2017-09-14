1dccae00cfc12f8044cd374fef603908dad66410
WanderingBrooks <cs.jasonbrooks@gmail.com>
2017-09-13 23:29:15 -0400

    Search fixed

--- handler.js
+++ handler.js
@@ -3,13 +3,24 @@
 const request = require('request');
 
 module.exports.malProxy = (event, context, callback) => {
-  
-  const response = {
-      statusCode: 200,
-      headers: {
-        'content-type': "application/x-www-form-urlencoded"
-      }
-  };
+
+  console.log(event);
+  console.log(context);
+  console.log(callback);
+
+  function handleResponse(error, res,body) {
+    const response = {
+        statusCode: 200,
+        headers: {
+          'content-type': "application/x-www-form-urlencoded"
+        }
+    };
+    console.log(error);
+    console.log(body);
+    response['body'] = body;
+    console.log(response);
+    callback(null, response);
+  }
 
   const type = event.queryStringParameters.type;
   const suffix = {
@@ -27,34 +38,28 @@ module.exports.malProxy = (event, context, callback) => {
   console.log(params.url);
 
   if (type === 'search') {
-    params.qs = event.queryStringParameters.data;
+    console.log("search");
+    params.qs = {
+      q: event.queryStringParameters.q
+    };
+    console.log(params.qs);
 
-    request(params, function (error, response, body) {
-      console.log(error);
-      console.log(body);
-      response['body'] = body;
-      console.log(response);
-      callback(null, response);
-    });
+    request(params, handleResponse);
   }
   else if (type === 'verify') {
-    request(params, function (error, res, body) {
-      console.log(error);
-      console.log(body);
-      response['body'] = body;
-      console.log(response);
-      callback(null, response);
-    }); 
+    console.log("verify");
+    request(params, handleResponse); 
   }
-  else if (type === 'add' || type === 'update' || type === 'delete') { 
-    params.url += `${event.queryStrigParameters.id}.xml`
+  else if (type === 'add' || type === 'update' ) { 
+    console.log(type);
+    params.url += `${event.queryStrigParameters.id}.xml`;
 
-    request.post(params, {form: {data: event.queryStringParameters.data}}, function (error, response, body) {
-      console.log(error);
-      console.log(body);
-      response['body'] = body;
-      console.log(response);
-      callback(null, response);
-    });
+    request.post(params, {form: {data: event.queryStringParameters.data}}, handleResponse);
+  }
+  else if (type === 'delete') { 
+    console.log(type);
+    params.url += `${event.queryStrigParameters.id}.xml`;
+
+    request.post(params, handleResponse);
   }
 }
